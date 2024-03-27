fb38cd6eafeef33a87ab1703846ae3196d8f10ac
qing.zhu <flowercode@dingtalk.com>
2024-03-27 20:35:21 +0800

    update apple sdl2 to lastest version

--- scripts/apple/sdl.sh
+++ scripts/apple/sdl.sh
@@ -1,47 +1,46 @@
 #!/bin/bash
 
-# SET BUILD OPTIONS
-ASM_OPTIONS=""
+# ALWAYS CLEAN THE PREVIOUS BUILD
+git clean -dfx 2>/dev/null 1>/dev/null
+
+echo "new sdl2 source copying place holder" > COPYING.txt
+
+# OVERRIDE SYSTEM PROCESSOR
+PLATFORM=""
 case ${ARCH} in
-*-mac-catalyst)
-  ASM_OPTIONS="--disable-video-cocoa --disable-render-metal --disable-diskaudio"
+arm64 | arm64e)
+  # OS64 or SIMULATORARM64
+  PLATFORM="OS64"
+  ;;
+arm64-simulator)
+  PLATFORM="SIMULATORARM64"
+  ;;
+x86-64)
+  # SIMULATOR64
+  PLATFORM="SIMULATOR64"
   ;;
 esac
 
-# ALWAYS CLEAN THE PREVIOUS BUILD
-make distclean 2>/dev/null 1>/dev/null
+# ios-cmake 目录与 ffmpeg-kit 目录平级
 
-# WORKAROUND TO FIX AUTOMATICALLY ENABLED LIBRARIES IN CONFIGURE
-overwrite_file "${BASEDIR}/tools/patch/make/sdl/configure.in" "${BASEDIR}/src/${LIB_NAME}/configure.in"
+mkdir build
+cd build
 
-# ALWAYS REGENERATE BUILD FILES - NECESSARY TO APPLY THE WORKAROUNDS
-./autogen.sh || return 1
-
-# WORKAROUND TO EXCLUDE libunwind.h ON LTS BUILDS
-if [[ -n ${FFMPEG_KIT_LTS_BUILD} ]]; then
-  export ac_cv_header_libunwind_h=no
-fi
-
-# UPDATE CONFIG FILES TO SUPPORT APPLE ARCHITECTURES
-overwrite_file "${FFMPEG_KIT_TMPDIR}"/source/config/config.guess "${BASEDIR}"/src/"${LIB_NAME}"/build-scripts/config.guess || return 1
-overwrite_file "${FFMPEG_KIT_TMPDIR}"/source/config/config.sub "${BASEDIR}"/src/"${LIB_NAME}"/build-scripts/config.sub || return 1
-
-./configure \
-  --prefix="${LIB_INSTALL_PREFIX}" \
-  --with-pic \
-  --with-sysroot="${SDK_PATH}" \
-  --enable-static \
-  --disable-shared \
-  --disable-video-opengl \
-  --disable-video-x11 \
-  --disable-joystick \
-  --disable-haptic \
-  ${ASM_OPTIONS} \
-  --host="${HOST}" || return 1
+cmake ../ -Wno-dev \
+ -G "Unix Makefiles" \
+ -DCMAKE_TOOLCHAIN_FILE=../../../ios-cmake/ios.toolchain.cmake \
+ -DPLATFORM="${PLATFORM}" \
+ -DBUILD_SHARED_LIBS=OFF \
+ -DBUILD_PROGRAMS=OFF \
+ -DCMAKE_INSTALL_PREFIX="${LIB_INSTALL_PREFIX}" \
+ -DDEPLOYMENT_TARGET=11.0 || return 1
 
 make -j$(get_cpu_count) || return 1
 
 make install || return 1
 
-# MANUALLY COPY PKG-CONFIG FILES
-cp ./*.pc "${INSTALL_PKG_CONFIG_DIR}" || return 1
+cd ..
+
+# CREATE PACKAGE CONFIG MANUALLY
+
+cp ${LIB_INSTALL_PREFIX}/lib/pkgconfig/sdl2.pc ${LIB_INSTALL_PREFIX}/../pkgconfig/sdl2.pc || return 1
